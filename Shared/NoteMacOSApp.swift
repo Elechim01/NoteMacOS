7b865ee911518116dec38f0bc89d86093e1d2595
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-17 19:40:20 +0200

    Initial Commit

---
+++ Shared/NoteMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteMacOSApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 17/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NoteMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
